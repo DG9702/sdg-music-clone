e290ddd0474ddb35659a715298b8cf76bb064510
Haibt9702 <buihaibq9702@gmail.com>
2023-12-13 20:55:11 +0700

    update index.scss commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import './App.css'
-
 function App() {
   return (
     <>
-      <div>
-        SDG
-      </div>
+      <div>SDG</div>
       <h1>Vite + React</h1>
-      <div className="card">
-        Hello world
-      </div>
+      <div>Hello world</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
