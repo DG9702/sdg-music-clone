817673995b0f08309323609fce9bd9d62ebc7ee5
Haibt9702 <buihaibq9702@gmail.com>
2023-12-13 20:04:30 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        SDG
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        Hello world
+      </div>
+    </>
+  )
+}
+
+export default App
